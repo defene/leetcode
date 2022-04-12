a71280f0fda54aa9f5768c56c44a6a03a4b6da7f
defene <92047614+defene@users.noreply.github.com>
2022-04-12 20:00:20 +0800

    Time: 108 ms (91.99%), Space: 22.3 MB (41.12%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.c
@@ -0,0 +1,14 @@
+
+
+void rotate(int* nums, int numsSize, int k){
+    k = k % numsSize;
+    if(k >= numsSize || k == 0) return;
+    int swap;
+    for(int i = 0; i < k; i++){
+        swap = nums[i];
+        nums[i] = nums[numsSize - k + i];
+        nums[numsSize - k + i] = swap;
+    }
+    
+    rotate(nums + k, numsSize - k, k);
+}
