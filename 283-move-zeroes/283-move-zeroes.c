c978f16eebe517d05cf517c054325da4e2ecdcae
defene <92047614+defene@users.noreply.github.com>
2022-07-07 13:21:54 +0800

    Time: 166 ms (24.53%), Space: 15.1 MB (53.36%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.c
+++ 283-move-zeroes/283-move-zeroes.c
@@ -6,7 +6,8 @@ void moveZeroes(int* nums, int numsSize){
         if(nums[i] != 0){
             int temp = nums[i];
             nums[i] = nums[j];
-            nums[j++] = temp;
+            nums[j] = temp;
+            j++;
         }
     }
 }
