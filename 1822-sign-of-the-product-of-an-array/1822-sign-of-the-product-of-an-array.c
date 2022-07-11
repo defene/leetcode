a02be96d1d655eb4c556dc0f5df32559b67ad0dc
defene <92047614+defene@users.noreply.github.com>
2022-07-11 08:46:53 +0800

    Time: 9 ms (50.52%), Space: 6.3 MB (37.63%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.c
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.c
@@ -2,14 +2,12 @@
 
 int arraySign(int* nums, int numsSize){
     int signNeg = 0;
-    int signZero = 0;
     
     for(int i = 0; i < numsSize; i++){
-        if(nums[i] == 0) signZero++;
+        if(nums[i] == 0) return 0;
         else if(nums[i] < 0) signNeg++;
     }
     
-    if(signZero) return 0;
-    else if(signNeg % 2) return -1;
+    if(signNeg % 2) return -1;
     else return 1;
 }
