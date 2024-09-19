ed72478327a6970a0a37898c3f894fe098a0cfc1
Chen Yiheng <92047614+defene@users.noreply.github.com>
2024-09-20 07:16:22 +0800

    Time: 29 ms (71.85%), Space: 52.2 MB (32.84%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> result = new LinkedList<>();
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             int left = i + 1;
             int right = nums.length - 1;
