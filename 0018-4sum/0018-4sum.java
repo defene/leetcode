7ef938854a1d7881ef6acd3e19fe317cb5855faa
Chen Yiheng <92047614+defene@users.noreply.github.com>
2024-09-20 10:30:47 +0800

    Time: 18 ms (60.52%), Space: 44 MB (44.54%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -14,8 +14,8 @@ class Solution {
                     if (sum == target) {
                         result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
                         while (left < right && nums[left + 1] == nums[left]) left++;
-                        left++;
                         while (left < right && nums[right - 1] == nums[right]) right--;
+                        left++;
                         right--;
                     } else if (sum < target) {
                         left++;
