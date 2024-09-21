fdffe365d9edd881109f5a64819021a1406738cc
Chen Yiheng <92047614+defene@users.noreply.github.com>
2024-09-21 09:25:19 +0800

    Time: 10 ms (76.14%), Space: 56.1 MB (94.89%) - LeetHub

---
+++ 1831-ways-to-split-array-into-three-subarrays/1831-ways-to-split-array-into-three-subarrays.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int waysToSplit(int[] nums) {
+        int MOD = 1000000007;
+        int n = nums.length;
+        long[] prefixSum = new long[n];
+
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+
+        int count = 0;
+        int minMid = 1, maxMid = 1;
+        for (int left = 0; left < n - 2; left++) {
+            while (minMid <= left || (minMid < n && prefixSum[minMid] - prefixSum[left] < prefixSum[left])) {
+                minMid++;
+            }
+            maxMid = Math.max(maxMid, minMid);
+            while (maxMid < n - 1 && prefixSum[maxMid] - prefixSum[left] <= prefixSum[n - 1] - prefixSum[maxMid]) {
+                maxMid++;
+            }
+            if (minMid <= maxMid - 1) {
+                count = (count + (maxMid - minMid)) % MOD;
+            }
+        }
+
+        return count;
+    }
+}
