5e62750466fcaec6d6f4e747dfe921f96c1383ae
Chen Yiheng <92047614+defene@users.noreply.github.com>
2024-10-26 23:05:33 +0800

    Time: 6 ms (57.98%), Space: 45.9 MB (50.24%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] inDegree = new int[numCourses];
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            int edgeFrom = prerequisites[i][1];   
+            int edgeTo = prerequisites[i][0];   
+            inDegree[edgeTo]++; 
+
+            if (!map.containsKey(edgeFrom)) {
+                List<Integer> adjEdges = new ArrayList<>();
+                adjEdges.add(edgeTo);
+                map.put(edgeFrom, adjEdges);
+            } else {
+                List<Integer> adjEdges = map.get(edgeFrom);
+                adjEdges.add(edgeTo);
+            }
+        }
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int i = 0;
+        int[] result = new int[numCourses];
+        while (!queue.isEmpty()) {
+            int curVertex = queue.poll();
+            result[i++] = curVertex;
+
+            List<Integer> adjEdges = map.get(curVertex);
+            if (adjEdges != null) {
+                for (int edgeTo : adjEdges) {
+                    inDegree[edgeTo]--;
+                    if (inDegree[edgeTo] == 0) {
+                        queue.offer(edgeTo);
+                    }
+                }
+            }
+        }
+
+        return i == numCourses ? result : new int[0];
+    }
+}
