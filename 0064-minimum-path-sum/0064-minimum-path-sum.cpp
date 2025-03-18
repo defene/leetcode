d7fdc6c95a2b1c1ac1a3b18f8110ea90f8756eb7
Yiheng Chen <92047614+defene@users.noreply.github.com>
2025-03-17 21:08:44 -0400

    Time: 0 ms (100%), Space: 15.1 MB (97.61%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -4,23 +4,19 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[0][0] = grid[0][0];
-        for (int i = 1; i < m; i++) {
-            dp[i][0] = dp[i - 1][0] + grid[i][0]; 
-        }
-        for (int i = 1; i < n; i++) {
-            dp[0][i] = dp[0][i - 1] + grid[0][i];
-        }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
-                printf("%d ", dp[i][j]);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+                else if (i == 0)
+                    grid[i][j] += grid[i][j - 1];
+                else if (j == 0)
+                    grid[i][j] += grid[i - 1][j];
+                else
+                    grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
             }
-            printf("\n");
         }
 
-        return dp[m - 1][n - 1];
+        return grid[m - 1][n - 1];
     }
 };
