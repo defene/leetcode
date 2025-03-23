d750b36fd05ea59689eef34ee0bec07c797b73af
Yiheng Chen <92047614+defene@users.noreply.github.com>
2025-03-23 09:18:25 -0400

    Time: 1 ms (99.46%), Space: 45.3 MB (40.33%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,25 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
+        int result = 0;
         int n = nums.length;
-        int[] dp = new int[n];
-        dp[n - 1] = 0;
+        int curEnd = 0;
+        int curFar = 0;
 
         for (int i = 0; i < n - 1; i++) {
-            dp[i] = Integer.MAX_VALUE;
+            curFar = Math.max(curFar, i + nums[i]);
+
+            if (i == curEnd) {
+                result++;
+                curEnd = curFar;
+            }
         }
 
-        for (int i = n - 2; i >= 0; i--) {
-            if (i + nums[i] >= n - 1) {
-                dp[i] = 1;
-                continue;
-            }
-            
-            for (int j = i + 1; j <= i + nums[i]; j++) {
-                dp[i] = Math.min(dp[j], dp[i]);
-            }
-            if (dp[i] != Integer.MAX_VALUE) dp[i]++;
-        }
-
-        return dp[0];
+        return result;
     }
 }
