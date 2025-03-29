2f34ab45072a39cf8c34462f7a84061982ae5533
Yiheng Chen <92047614+defene@users.noreply.github.com>
2025-03-29 13:53:24 -0400

    Time: 11 ms (51.21%), Space: 11.4 MB (81.01%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        if (s.length() == 0 || t.length() == 0) {
+            return "";
+        }
+
         unordered_map<char, int> map;
         for (char c : t) {
             map[c]++;
         }
         
-        int result = INT_MAX;
         int windowLeft = 0;
-        int start = 0;
         int matched = 0;
-        
+        int start = 0;
+        int result = INT_MAX;
         for (int windowRight = 0; windowRight < s.length(); windowRight++) {
             char cur = s[windowRight];
-            
             if (map.find(cur) != map.end()) {
                 map[cur]--;
                 if (map[cur] >= 0) {
@@ -34,6 +36,7 @@ public:
                         matched--;
                     }
                 }
+
                 windowLeft++;
             }
         }
